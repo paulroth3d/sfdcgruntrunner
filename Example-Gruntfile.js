9da0d813c6ba404c51e5e4e7dcfc25268cec16e9
Paul Roth <proth@salesforce.com>
2014-12-18 14:23:10 -0600

    UPDATE: add in more comments to the example package and grunt file.

--- Example-Gruntfile.js
+++ Example-Gruntfile.js
@@ -1,12 +1,23 @@
 /*global require, chalk, module*/
 module.exports = function(grunt) {
 	
+	//-- update this to /path/to/tasks folder
+	grunt.loadTasks( '../sfdcgruntrunner/tasks' );
+	
 	//-- update this to /path/to/grunt-sfdc-runner.js
-	require( "./grunt-sfdc-runner.js" )(grunt);
+	require( "../sfdcgruntrunner/grunt-sfdc-runner.js" )(grunt);
 	
 	//-- override any configuration here as desired
-	//-- see 
+	//-- see following for a list of jshint options - http://jshint.com/docs/options
+	//-- NOTE: the files combined and ordering are within the package.xml file
+	/** Example config setting.
 	grunt.config.merge({
+		"jshint":{
+			"options":{
+				node:true,
+				newcap:false
+			}
+		},
 		"uglify":{
 			sfdc_uglify: {
 				options: {
@@ -15,4 +26,5 @@ module.exports = function(grunt) {
 			}
 		}
 	});
+	*/
 };
